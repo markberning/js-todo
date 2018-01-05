462ee5b3fadf0d5e9594d66383f8e9a33f8ef686
Mark Berning <mebernin@gmail.com>
2018-01-04 21:18:38 -0800

    edited comments for newly added functions

--- v2/script.js
+++ v2/script.js
@@ -8,27 +8,26 @@ change todos,
 and delete todos
 */
 
-/* 1. It should have a place to store todos. */
 var todos = ['item 1'];
 
-/* 2. It should have a way to display todos. */
+/* 1a. It should have a function to display todos. */
 function displayTodos(){
   console.log('My Todos:', todos);
 }
 
-/* 3. It should have a way to add new todos. */
+/* 1b. It should have a function to add new todos. */
 function addTodo(todo){
   todos.push(todo);
   displayTodos();
 }
 
-/* 4. It should have a way to change a todo. */
+/* 1c. It should have afunction to change a todo. */
 function editTodo(position, todo){
   todos[position] = todo;
   displayTodos();
 }
 
-/* 5. It should have a way to delete a todo. */
+/* 1d. It should have a function to delete a todo. */
 function deleteTodo(position){
   todos.splice(position, 1);
   displayTodos();
